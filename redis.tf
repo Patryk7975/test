e3576b7516b8804871f05f5bc3f6a88fbd7742a8
Patryk7975 <52758327+Patryk7975@users.noreply.github.com>
2021-04-06 09:01:21 +0200

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -1,4 +1,13 @@
-resource "google_redis_instance" "cache" {
-  name           = "memory-cache"
-  memory_size_gb = 1
+resource "google_storage_bucket" "pko-nrss-bucket" {
+    name = "bucket1"
+    storage_class = "STANDARD"   
+    location = "europe-north1"  
+    
+    encryption {
+        default_kms_key_name = "projects/terraform-lab-293713/locations/europe-north1/keyRings/testring/cryptoKeys/testkey"   
+    }
+
+    versioning {
+        enabled = true
+    }
 }
